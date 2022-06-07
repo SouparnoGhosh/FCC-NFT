95fc24aba40551b537eed54c15b07bbd4c8d8a2a
SouparnoGhosh <ghosh99souparno@gmail.com>
2022-06-07 10:54:16 +0530

    tests added

---
+++ test/basicNft.test.ts
@@ -0,0 +1,30 @@
+/* eslint-disable node/no-missing-import */
+/* eslint-disable no-unused-expressions */
+import { expect } from "chai";
+import { developmentChains, networkConfig } from "../helper-hardhat-config";
+import { ethers, network, deployments } from "hardhat";
+import { BasicNft } from "../typechain";
+
+!developmentChains.includes(network.name)
+  ? describe.skip
+  : describe("Basic NFT Unit Tests", function () {
+      let basicNft: BasicNft;
+      let deployer;
+
+      beforeEach(async () => {
+        const accounts = await ethers.getSigners();
+        deployer = accounts[0];
+        await deployments.fixture(["mocks", "basicNft"]);
+        basicNft = await ethers.getContract("BasicNft");
+      });
+
+      it("Allows users to mint an NFT, and updates appropriately", async function () {
+        const txResponse = await basicNft.mintNft();
+        await txResponse.wait();
+        const tokenURI = await basicNft.tokenURI(0);
+        const tokenCounter = await basicNft.getTokenCounter();
+
+        expect(await basicNft.TOKEN_URI()).to.eq(tokenURI);
+        expect(tokenCounter.toString()).to.eq("1");
+      });
+    });
