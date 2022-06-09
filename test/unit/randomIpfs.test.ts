523586216bddcdc0fca605aecbd14fc09f1e23b2
SouparnoGhosh <ghosh99souparno@gmail.com>
2022-06-09 21:21:58 +0530

    constructor test

--- test/unit/randomIpfs.test.ts
+++ test/unit/randomIpfs.test.ts
@@ -1,6 +1,32 @@
 /* eslint-disable node/no-missing-import */
 /* eslint-disable no-unused-expressions */
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { assert, expect } from "chai";
 import { network, deployments, ethers } from "hardhat";
 import { developmentChains, networkConfig } from "../../helper-hardhat-config";
 import { RandomIpfsNft, VRFCoordinatorV2Mock } from "../../typechain";
+
+!developmentChains.includes(network.name)
+  ? describe.skip
+  : describe("Random IPFS NFT Unit Tests", function () {
+      let randomIpfsNft: RandomIpfsNft,
+        deployer: SignerWithAddress,
+        vrfCoordinatorV2Mock: VRFCoordinatorV2Mock;
+
+      beforeEach(async () => {
+        const accounts = await ethers.getSigners();
+        deployer = accounts[0];
+        await deployments.fixture(["mocks", "randomipfs"]);
+        randomIpfsNft = await ethers.getContract("RandomIpfsNft");
+        vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock");
+      });
+
+      describe("constructor", function () {
+        it("sets starting values correctly", async function () {
+          const dogTokenUriZero = await randomIpfsNft.getDogTokenUris(0);
+          const isInitialized = await randomIpfsNft.getInitialized();
+          assert(dogTokenUriZero.includes("ipfs://"));
+          assert.equal(isInitialized, true);
+        });
+      });
+    });
