e1ee2da2aa7c7e8c5a2a297cd34fc454310f7d53
SouparnoGhosh <ghosh99souparno@gmail.com>
2022-06-09 21:43:14 +0530

    finish tests

--- test/unit/randomIpfs.test.ts
+++ test/unit/randomIpfs.test.ts
@@ -29,4 +29,53 @@ import { RandomIpfsNft, VRFCoordinatorV2Mock } from "../../typechain";
           assert.equal(isInitialized, true);
         });
       });
+
+      describe("requestNft", function () {
+        it("fails if payment isn't sent with the request", async function () {
+          await expect(randomIpfsNft.requestNft()).to.be.revertedWith(
+            "NeedMoreETHSent"
+          );
+        });
+
+        it("emits and event and kicks off a random word request", async function () {
+          const fee = await randomIpfsNft.getMintFee();
+          await expect(
+            randomIpfsNft.requestNft({ value: fee.toString() })
+          ).to.emit(randomIpfsNft, "NftRequested");
+        });
+      });
+
+      describe("fulfillRandomWords", function () {
+        it("mints NFT after random number returned", async function () {
+          // eslint-disable-next-line no-async-promise-executor
+          await new Promise<void>(async (resolve, reject) => {
+            randomIpfsNft.once("NftMinted", async () => {
+              try {
+                const tokenUri = await randomIpfsNft.tokenURI(0);
+                const tokenCounter = await randomIpfsNft.getTokenCounter();
+                assert.equal(tokenUri.toString().includes("ipfs://"), true);
+                assert.equal(tokenCounter.toString(), "1");
+                resolve();
+              } catch (error: any) {
+                reject(error);
+              }
+            });
+
+            try {
+              const fee = await randomIpfsNft.getMintFee();
+              const requestNftResponse = await randomIpfsNft.requestNft({
+                value: fee.toString(),
+              });
+              const requestNftReceipt = await requestNftResponse.wait(1);
+              await vrfCoordinatorV2Mock.fulfillRandomWords(
+                requestNftReceipt.events![1].args!.requestId,
+                randomIpfsNft.address
+              );
+            } catch (error: any) {
+              console.log(error);
+              reject(error);
+            }
+          });
+        });
+      });
     });
